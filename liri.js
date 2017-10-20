30f0e7a2f140d550fd9784723b1f173ac373b67a
Caromin <c.aromin.unf@gmail.com>
2017-10-20 01:40:20 -0400

    reattempted some small problems, unable to test api calls, they all need proper api keys/tokens now for access to info

--- liri.js
+++ liri.js
@@ -1,32 +1,23 @@
-var request = require("request");
-var fs = require("fs");
-var twitter = require("twitter");
+// Import the Keys file
 var keys = require("./keys.js");
+
+// Import the Twitter NPM package.
+var Twitter = require("twitter");
+
+// Import the Spotify npm package.
 var spotify = require("spotify");
 
-var type = process.argv[2];
-var empty = "";
-//used for the movie rottentomatoesURL, would not push
-var emptyArray = [];
-// console.log("Process.argv array length: " + process.argv.length);
+// Import the request npm package.
+var request = require("request");
 
-//set up for multi word process.argv
-for (i=3; i<process.argv.length; i++) {
-	if (i>3) {
-		var test = process.argv[i];
-		empty +=  "+" + test;
-		// emptyArray.push(test);
-		// emptyArray.join("_"); 
-	}	
-//else just run single word	
-	else { empty = process.argv[3];
-		emptyArray = process.argv[3];}
-}
-// console.log("empty string check: " + empty);
+// Import the FS package for read/write.
+var fs = require("fs");
+
+var type = process.argv[2];
 
 //if statements to determine which function to run
 switch (type) {
-	case "move-this":
+	case "movie-this":
 		movies();
 		break;
 	case "my-tweets":
@@ -37,22 +28,47 @@ switch (type) {
 		break;
 	default:
 		console.log("none of the functions ran, you failed");
-		
+};		
 
 
-
+//NOT ACCESSIBLE ANYMORE, NEED API NEED TO PAY, BUT WORKING	
 //function to run movies npm
 function movies() {
+	emptyArray = [];
+	ArraytoString = '';
+	urlLink = '';
 //if nothing is inputed after argv[2], will auto fill "Mr. Nobody"	
 	if (process.argv[3] === undefined) {
-		var empty = "Mr. Nobody";
-		emptyArray = "Mr._Nobody"; }
-//request for movie info		
-		request("http://www.omdbapi.com/?t=" + empty, function(error, response, body) {
-    	console.log("\nYou searched for: " + JSON.parse(body).Title +"\nYear: " + JSON.parse(body).Year + '\nRating: ' + JSON.parse(body).imdbRating
-    		+ '\nCountry: ' + JSON.parse(body).Country + '\nLanguage: ' + JSON.parse(body).Language + '\nPlot: ' + JSON.parse(body).Plot
-    		+ '\nActors: ' + JSON.parse(body).Actors + '\nRottenTomatoesRating: ' + JSON.parse(body).Ratings + '\nRottenTomatoesURL: www.rottentomatoes.com/m/' + emptyArray);
-});
+		ArraytoString = "Mr. Nobody";
+	} else if (process.argv.length > 2) { 
+		for (i = 3; i < process.argv.length; i++ ) {
+		emptyArray.push(process.argv[i]);
+		// console.log(emptyArray.toString());
+		}
+		ArraytoString = emptyArray.toString();
+		urlLink = ArraytoString.replace(/,/g, "_");
+		console.log('string:' + ArraytoString);
+		console.log('urllink:' + urlLink);
+
+	} else {
+		ArraytoString = process.argv[3].toString();
+	}
+
+//request for movie info
+//api is now private does not show info undefined	
+		request("http://www.omdbapi.com/?t=" + ArraytoString, function(error, response, body) {
+    	console.log("\nYou searched for: " + 
+    		JSON.parse(body).Title +"\nYear: " + 
+    		JSON.parse(body).Year + '\nRating: ' + 
+    		JSON.parse(body).imdbRating + '\nCountry: ' + 
+    		JSON.parse(body).Country + '\nLanguage: ' + 
+    		JSON.parse(body).Language + '\nPlot: ' + 
+    		JSON.parse(body).Plot + '\nActors: ' + 
+    		JSON.parse(body).Actors + '\nRottenTomatoesRating: ' + 
+    		JSON.parse(body).Ratings + '\nRottenTomatoesURL: www.rottentomatoes.com/m/' + 
+    		urlLink
+    	);
+	});
 }
 
 //function to run twitter npm
@@ -71,19 +87,29 @@ function twitter() {
 });
 }
 
-
+//DOES NOT WORK NEEDS TOKEN ALSO FOR API ACCESS
+//Found out by using JSON.stringify(data) in console.log to found out.
 //function to run spotify npm
 function songs() {
+	empty = '';
+	fs.readFile("random.txt", "utf8", function(error, transferedData) {
+    	console.log('transferedData: ' + transferedData);
+    });
 //if nothing is inputed after argv[2], will auto fill "The Sign"	
 	if (process.argv[3] === undefined) {
-		var empty = "The Sign by Ace of Base";
+		empty = "The Sign by Ace of Base";
+		console.log(empty);
 	}
 	spotify.search({ type: 'track', query: empty }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;}
-//request for song info	        
-	else {console.log("\nYou searched for (First Result): " + data.tracks.items[0].name + "\nArtist: " + data.tracks.items[0].artists[0].name
-			+ "\nPreview Link: " + data.tracks.items[0].preview_url + "\nAlbum: " + data.tracks.items[0].album.name);}
-});
+   	if ( err ) {
+        	console.log('Error occurred: ' + err);
+        	return;
+     	} else {
+  	   	console.log("\nYou searched for (First Result): " + data.tracks.items[0].name + 
+			"\nArtist: " + data.tracks.items[0].artists[0].name + 
+			"\nPreview Link: " + data.tracks.items[0].preview_url + 
+			"\nAlbum: " + data.tracks.items[0].album.name
+			);
+  	  	}
+	});
 }
